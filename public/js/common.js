815bbce5c613a737116fca9cf2e5f2bc97be2d88
Aleksandr3012 <flaming541@gmail.com>
2021-04-07 13:54:25 +0300

    edits

--- public/js/common.js
+++ public/js/common.js
@@ -419,6 +419,31 @@ function eventHandler() {
 		});
 		wow.init();
 	};
+
+	var show = true;
+	var countbox = ".sAboutText";
+	$(window).on("scroll load resize", function () {
+		if (!show) return false; // Отменяем показ анимации, если она уже была выполнена
+
+		var w_top = $(window).scrollTop(); // Количество пикселей на которое была прокручена страница
+
+		var e_top = $(countbox).offset().top; // Расстояние от блока со счетчиками до верха всего документа
+
+		var w_height = $(window).height(); // Высота окна браузера
+
+		var d_height = $(document).height(); // Высота всего документа
+
+		var e_height = $(countbox).outerHeight(); // Полная высота блока со счетчиками
+
+		if (w_top + 500 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
+			$('.counter-js').css('opacity', '1');
+			$('.counter-js').spincrement({
+				thousandSeparator: "",
+				duration: 2000
+			});
+			show = false;
+		}
+	});
 }
 
 ;
