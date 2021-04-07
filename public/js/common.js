478bee639f7f50de86d4cf9c0b73baa3e8917c1a
Aleksandr3012 <flaming541@gmail.com>
2021-04-07 18:24:06 +0300

    textarea

--- public/js/common.js
+++ public/js/common.js
@@ -420,30 +420,34 @@ function eventHandler() {
 		wow.init();
 	};
 
-	var show = true;
-	var countbox = ".sAboutText";
-	$(window).on("scroll load resize", function () {
-		if (!show) return false; // Отменяем показ анимации, если она уже была выполнена
+	if ($('.counter-js')) {
+		var show = true;
+		var countbox = ".sAboutText";
+		$(window).on("scroll load resize", function () {
+			if (!show) return false; // Отменяем показ анимации, если она уже была выполнена
 
-		var w_top = $(window).scrollTop(); // Количество пикселей на которое была прокручена страница
+			var w_top = $(window).scrollTop(); // Количество пикселей на которое была прокручена страница
 
-		var e_top = $(countbox).offset().top; // Расстояние от блока со счетчиками до верха всего документа
+			var e_top = $(countbox).offset().top; // Расстояние от блока со счетчиками до верха всего документа
 
-		var w_height = $(window).height(); // Высота окна браузера
+			var w_height = $(window).height(); // Высота окна браузера
 
-		var d_height = $(document).height(); // Высота всего документа
+			var d_height = $(document).height(); // Высота всего документа
 
-		var e_height = $(countbox).outerHeight(); // Полная высота блока со счетчиками
+			var e_height = $(countbox).outerHeight(); // Полная высота блока со счетчиками
 
-		if (w_top + 500 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
-			$('.counter-js').css('opacity', '1');
-			$('.counter-js').spincrement({
-				thousandSeparator: "",
-				duration: 2000
-			});
-			show = false;
-		}
-	});
+			if (w_top + 500 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
+				$('.counter-js').css('opacity', '1');
+				$('.counter-js').spincrement({
+					thousandSeparator: "",
+					duration: 2000
+				});
+				show = false;
+			}
+		});
+	}
+
+	;
 }
 
 ;
